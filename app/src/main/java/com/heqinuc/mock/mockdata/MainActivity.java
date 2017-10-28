0585444e63aa283cf8b9d866d4ef31fb857debed
kapaseker <panoogunker@gmail.com>
2017-10-28 20:36:01 +0800

    update gradle version

--- app/src/main/java/com/heqinuc/mock/mockdata/MainActivity.java
+++ app/src/main/java/com/heqinuc/mock/mockdata/MainActivity.java
@@ -26,9 +26,9 @@ import static com.heqinuc.mock.mockdata.background.DataServiceGenerator.createOn
 
 public class MainActivity extends AppCompatActivity {
 
-	public static final int DATA_COUNT = 4466;
-	public static final int SLEEP_INTERVER = 2;
-	public static final String FILE_NAME = "hunankey4467.data";
+	public static final int DATA_COUNT = 6000;
+	public static final int SLEEP_INTERVER = 1;
+	public static final String FILE_NAME = "tongji6000.data";
 
 	TextView mTxtInfo = null;
 	OnlineDownloadService mService = null;
